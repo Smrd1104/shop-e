e62acd5985b564debb20c18ba3e6e244c4cd9af9
Smrd1104 <mriyasdeen1104@gmail.com>
2024-07-13 19:40:53 +0530

    password shown icon login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import Logo from "../../assets/logo.png";
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false); // State to manage password visibility
 
     const navigate = useNavigate();
 
@@ -18,16 +22,19 @@ const Login = () => {
                     localStorage.setItem('isAuthenticated', true);
                     navigate('/');
                 }
-
             })
             .catch(err => {
                 console.log(err);
             });
     }
 
+    // Function to toggle password visibility
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
         <div className="relative flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900 overflow-hidden">
-
             <div className="absolute inset-0 z-0 w-full h-full bg-gradient-to-r from-primary to-secondary/40 dark:bg-black"></div>
             <div className="absolute w-[700px] h-[700px] bg-gradient-to-r from-primary to-secondary/40 rounded-3xl dark:bg-black rotate-45 top-1/2 right-0 transform -translate-y-1/2 translate-x-1/2"></div>
             <div className='flex flex-col gap-5 '>
@@ -46,17 +53,26 @@ const Login = () => {
                                 onChange={(e) => setEmail(e.target.value)}
                                 type="email"
                                 id="email"
-                                className="w-full lg:py-3  lg:pl-2 p-3 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
+                                className="w-full lg:py-3 lg:pl-2 p-3 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
                             />
                         </div>
                         <div className="mb-4">
                             <label className="block text-gray-700 dark:text-gray-300 mb-2" htmlFor="password">Password</label>
-                            <input
-                                onChange={(e) => setPassword(e.target.value)}
-                                type="password"
-                                id="password"
-                                className="w-full lg:py-3 lg:pl-2 p-3  border rounded-lg dark:bg-gray-700 dark:text-gray-300"
-                            />
+                            <div className="relative">
+                                <input
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    type={showPassword ? 'text' : 'password'}
+                                    id="password"
+                                    className="w-full lg:py-3 lg:pl-2 p-3 border rounded-lg dark:bg-gray-700 dark:text-gray-300"
+                                />
+                                <button
+                                    type="button"
+                                    onClick={togglePasswordVisibility}
+                                    className="absolute inset-y-0 lg:right-5 right-0 flex items-center pr-3"
+                                >
+                                    <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="text-gray-700 dark:text-gray-300" />
+                                </button>
+                            </div>
                         </div>
 
                         <button
@@ -66,16 +82,15 @@ const Login = () => {
                             Login
                         </button>
                         <div className='pt-4'>
-
                             <Link to='/sign-up'>
                                 <h3 className='pb-4 hover:text-red-500 hover:underline'>Create New account?</h3>
-                                <button
-                                    type="submit"
-                                    className="w-full bg-primary text-white p-3 rounded-lg"
-                                >
-                                    Signup
-                                </button>
                             </Link>
+                            <button
+                                type="submit"
+                                className="w-full bg-primary text-white p-3 rounded-lg"
+                            >
+                                Signup
+                            </button>
                         </div>
                     </form>
                 </div>
