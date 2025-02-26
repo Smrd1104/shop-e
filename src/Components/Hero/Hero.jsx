a3aaab23fc2de5df5b9e5221b1f996065b4f4e90
Smrd1104 <mriyasdeen1104@gmail.com>
2025-02-26 12:03:08 +0530

    changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ import image4 from "../../assets/vkc/hand-bag (2).png";
 import image5 from "../../assets/vkc/t-shirt (3).png";
 import image6 from "../../assets/hero/hero-image3.png";
 import image7 from "../../assets/furniture.png";
-// import image8 from "../../assets/hero-banner.jpg"
+import image8 from "../../assets/hero/headphone.avif"
 
 
 const ImageList = [
@@ -19,68 +19,60 @@ const ImageList = [
     id: 1,
     img: image1,
     title: "Upto 50% off on all Men's Sandals",
-    description:
-      "His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+     
   },
   {
     id: 2,
     img: image2,
     title: "Upto 70% off on all Women's Slipper",
-    description:
-      "His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+     
   },
   {
     id: 3,
     img: image3,
     title: "Upto 80% off on all Women's Hills",
-    description:
-      "His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+     
   },
   {
     id: 4,
     img: image4,
     title: "Upto 50% off on all Women's Hand Bags",
-    description:
-      "His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+     
   },
   {
     id: 5,
     img: image5,
     title: "Upto 60% off on all Men's t-shirts",
-    description:
-      "His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+     
   },
   {
     id: 6,
     img: image6,
     title: "More offers on all Products",
-    description:
-      "His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+     
   },
   {
     id: 7,
     img: image7,
     title: "Upto 50% off on all Household things",
-    description:
-      "His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+     
   },
-  // {
-  //   id: 8,
-  //   img: image8,
-  //   title: "Upto 50% off on all Men's Sandals",
-  //   description:
-  //     "His Life will forever be Changed dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  // },
+  {
+    id: 8,
+    img: image8,
+    title: "Upto 50% off on all Men's Sandals",
+     
+  },
 ];
 
 const Hero = () => {
   return (
-    <div className="relative overflow-hidden  bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 md:py-14">
+    <div className="relative overflow-hidden  bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200 ">
       {/* Background pattern */}
-      <div className="w-[700px] h-[700px] bg-gradient-to-r from-[#FCB714] via-[#ED8023] via-[#00A8CE] to-[#0F56A6]/50 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9 "></div>
+      <div className="md:w-[650px] w-[500px] h-[500px] md:h-[480px]  bg-gradient-to-r from-[#FCB714] via-[#ED8023] via-[#00A8CE] to-[#0F56A6]/60 absolute -top-1/2  md:-translate-y-8 right-0 rounded-3xl rotate-45 -z-9 "></div>
       
       {/* Hero Section */}
-      <div className="container pb-8 sm:pb-0">
+      <div className="container  sm:pb-0">
         <Swiper
           modules={[Autoplay, Pagination, Navigation]}
           spaceBetween={50}
@@ -109,12 +101,12 @@ const Hero = () => {
             <SwiperSlide key={data.id}>
               <div className="grid grid-cols-1 sm:grid-cols-2">
                 {/* Text Content */}
-                <div className="flex flex-col justify-center gap-4 md:pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                <div className="flex flex-col justify-center gap-4 sm:pt-0 text-center sm:text-left order-2  relative z-10">
                   <h1
                     data-aos="zoom-out"
                     data-aos-once="true"
                     data-aos-duration="500"
-                    className="md:text-[3.5rem] text-[2.5rem]   font-bold"
+                    className="md:text-[3.5rem] text-[1.5rem]   font-bold"
                   >
                     {data.title}
                   </h1>
@@ -147,7 +139,7 @@ const Hero = () => {
                     <img
                       src={data.img}
                       alt="product"
-                      className="md:w-[400px] w-[250px]   md:h-[400px] h-[300px]  sm:scale-125 lg:scale-125 object-contain mx-auto"
+                      className="md:w-[350px] w-[250px]   md:h-[400px] h-[250px]  sm:scale-125 lg:scale-125 object-contain mx-auto"
                     />
                   </div>
                 </div>
